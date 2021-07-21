293243870cff8e10cfeea3fe60048e5d92bd1a09
Logulasse <maxence.tasse@gmail.com>
2021-07-22 00:25:54 +0200

    Ajout de l'entité Menu

---
+++ src/Entity/Menu.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MenuRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MenuRepository::class)
+ */
+class Menu
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $idRef;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $path;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getIdRef(): ?int
+    {
+        return $this->idRef;
+    }
+
+    public function setIdRef(?int $idRef): self
+    {
+        $this->idRef = $idRef;
+
+        return $this;
+    }
+
+    public function getPath(): ?string
+    {
+        return $this->path;
+    }
+
+    public function setPath(string $path): self
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+}
